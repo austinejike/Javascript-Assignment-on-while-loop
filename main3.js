946dd229c81269dd5d86ed7474ce109e908cfd1d
austinejike <austinejike1@gmail.com>
2021-03-21 16:17:28 +0100

    Update main3.js

--- main3.js
+++ main3.js
@@ -2,10 +2,10 @@
 // "Are we there yet?". Keep asking again and 
 //again until they enter "yes" OR "yeah". Then, alert "Yah, we finally made it!"
 
-//let question = prompt("Are we there");
-//let ans = 'yes'
-//let ans2 = 'yeah'
-//while(question !== ans && (question !== ans2)){
-//    question = prompt("Are we there")
-//}
-//alert('yeah we made it')
+let question = prompt("Are we there");
+let ans = 'yes'
+let ans2 = 'yeah'
+while(question !== ans && (question !== ans2)){
+    question = prompt("Are we there")
+}
+alert('yeah we made it')
